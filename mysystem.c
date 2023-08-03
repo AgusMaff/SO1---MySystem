c0244c7dfd23a66a2d2185c1db62caf5f45b64b0
Agustin-Maffini <maffiniagustin@gmail.com>
2023-08-03 15:52:03 -0300

    .

--- mysystem.c
+++ mysystem.c
@@ -3,3 +3,28 @@
 //
 
 #include "mysystem.h"
+
+int mysystem(char* program, char** opt){
+    int status;
+    pid_t pid = fork();
+
+    switch(pid){
+        case 0:
+            if(execvp(program, opt) == -1){
+                fprintf(stderr,"No se pudo ejecutar el comando indicado. %d:%s\n", errno, strerror(errno));
+            }
+            break;
+        case -1:
+            fprintf(stderr,"Hubo un fallo durante la creacion del proceso hijo encargador de la ejecucion del comando. %d:%s\n", errno, strerror(errno));
+            break;
+        default:
+            signal(SIGINT,SIG_IGN);
+            signal(SIGQUIT,SIG_IGN);
+
+            waitpid(pid, &status, 0);
+
+            signal(SIGINT,SIG_DFL);
+            signal(SIGQUIT,SIG_DFL);
+    }
+    return 0;
+}
--- mysystem.h
+++ mysystem.h
@@ -4,5 +4,16 @@
 
 #ifndef SO1_MYSYSTEM_MYSYSTEM_H
 #define SO1_MYSYSTEM_MYSYSTEM_H
+#define SIZE 64
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int mysystem(char*, char**);
 
 #endif //SO1_MYSYSTEM_MYSYSTEM_H
